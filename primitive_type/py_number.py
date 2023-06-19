7a85763f4da622d20ce2e08d0d03ba9cb4dc447d
tsCalm <dlatkdals93@gmail.com>
2023-06-19 16:29:57 +0900

    feat: 파이썬 넘버타입 기본

---
+++ primitive_type/py_number.py
@@ -0,0 +1,18 @@
+val_int_one = 2 + 2
+val_int_two = 50 - 5*6
+val_float_one = (50 - 5*6) / 4
+val_float_two = 8 / 5  # division always returns a floating point number
+val_floor_division = 32//3
+print(val_floor_division)
+# print(val_int_one, val_int_two, val_float_one, val_float_two)
+
+val_float_int_calc = 13.0 -1
+print(val_float_int_calc)
+
+"""
+1. 계산식에서 '/'(Division)를 사용하면 리턴 타입은 float타입
+2. 계산식에서 '//'(Division)를 사용하면 리턴 타입은 number타입, 나머지는 버린다. ( floor )
+3. ** 거듭제곱, % 나머지 반환 % 은 자바스크립트와 유사
+4. float, int를 혼합해서 계산하면 반환형은 float
+5. 
+"""
