dde0ee59e5bfcac3fc9941e03c187da685602fad
tsCalm <dlatkdals93@gmail.com>
2023-06-21 18:16:12 +0900

    인자 목록 언패킹 딕셔너리

--- basic/py_def.py
+++ basic/py_def.py
@@ -113,3 +113,11 @@ def concat(*args, sep="/"):
 list(range(3, 6)) # normal call with separate arguments
 args = [3, 6]
 list(range(*args)) # call with arguments unpacked from a list
+
+def parrot(voltage, state='a stiff', action='voom'):
+    print("-- This parrot wouldn't", action, end=' ')
+    print("if you put", voltage, "volts through it.", end=' ')
+    print("E's", state, "!")
+
+d = {"voltage": "four million", "state": "bleedin' demised", "action": "VOOM"}
+parrot(**d)
